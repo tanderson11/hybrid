53187ea6f19a42f455ad8c702f8c1d07ef71b77a
tander64 <tander64@login01.cm.cluster>
2024-02-21 18:36:28 -0500

    Working test submission pipeline.

--- tests/clustertest.py
+++ tests/clustertest.py
@@ -7,13 +7,10 @@ import unit_tests
 if __name__ == '__main__':
     assert len(sys.argv) == 2, "Usage: 'python clustertest i' where i == ith test to run from SBML suite, arbitrarily sorted."
     i = int(sys.argv[1])
+    print("I", i)
     suite = unittest.TestLoader().loadTestsFromTestCase(unit_tests.TestSBML)
-    for j, test in enumerate(sorted(suite)):
-        if i != j:
-            continue
-        print(f'Running {i}={test}')
-        print(type(test))
-        runner = unittest.TextTestRunner()
-        runner.run(test)
-
+    suite_list = list(suite)
+    runner = unittest.TextTestRunner()
+    print(suite_list[i])
+    runner.run(suite_list[i])
 
