9912b888d8fb9b74d8b5462dcfa64912bb1e8301
Thayer Anderson <tanderson11@gmail.com>
2024-03-02 15:51:03 -0500

    Working refactor for Gillespie simulation.

--- hybrid/gillespie.py
+++ hybrid/gillespie.py
@@ -4,32 +4,124 @@ from numba import jit
 from scipy.integrate import quad
 from scipy.optimize import fsolve
 
-def jit_calculate_propensities_factory(kinetic_order_matrix):
-    # assumes constant rate constants
-    @jit(nopython=True)
-    def jit_calculate_propensities(y, k):
-        # Remember, we want total number of distinct combinations * k === rate.
-        # we want to calculate (y_i rate_involvement_ij) (binomial coefficient)
-        # for each species i and each reaction j
-        # sadly, inside a numba C function, we can't avail ourselves of scipy's binom,
-        # so we write this little calculator ourselves
-        intensity_power = np.zeros_like(kinetic_order_matrix)
-        for i in range(0, kinetic_order_matrix.shape[0]):
-            for j in range(0, kinetic_order_matrix.shape[1]):
-                if y[i] < kinetic_order_matrix[i][j]:
-                    intensity_power[i][j] = 0.0
-                elif y[i] == kinetic_order_matrix[i][j]:
-                    intensity_power[i][j] = 1.0
-                else:
-                    intensity = 1.0
-                    for x in range(0, kinetic_order_matrix[i][j]):
-                        intensity *= (y[i] - x) / (x+1)
-                    intensity_power[i][j] = intensity
-        product_down_columns = np.ones(len(k))
-        for i in range(0, len(y)):
-            product_down_columns = product_down_columns * intensity_power[i]
-        return product_down_columns * k
-    return jit_calculate_propensities
+from .simulators import Step, StepStatus, Simulator
+
+class GillespieStepStatus(StepStatus):
+    rejected = -1
+    t_end = 0
+    stochastic = 1
+
+class HittingTimeProposal(NamedTuple):
+    tau: float
+    propensities: np.ndarray = None
+    total_propensity: float = None
+
+def find_hitting_time_inhomogenous(t, y, propensity_function, rng):
+    hitting_point = rng.exponential(1)
+    f = inhomogeneous_upper_bound_f_factory(t, y, hitting_point, propensity_function)
+    hitting_time = fsolve(f, x0=1)
+    return HittingTimeProposal(hitting_time)
+
+def find_hitting_time_homogeneous(propensities, total_propensity, rng):
+    hitting_point = rng.exponential(1)
+    hitting_time = hitting_point / total_propensity
+    return HittingTimeProposal(hitting_time, propensities, total_propensity)
+
+def inhomogeneous_upper_bound_f_factory(t, y, hitting_point, propensity_function):
+    # we need to solve for the hitting time
+    # which is the time when hitting_point / integral of propensities
+    # so we build an objective function that takes a zero when x = hitting_point / integral
+    # using numerical minimization we can minimize the objective function to find the time x
+    def objective_function(x):
+        integral = quad(propensity_function, t, x, args=(y))[0]
+        return np.abs(x - hitting_point / integral)
+    return objective_function
+
+def gillespie_update_proposal(N, propensities, total_propensity, rng):
+    selections = propensities.cumsum() / total_propensity
+    pathway_rand = rng.random()
+    entry = np.argmax(selections > pathway_rand)
+    path_index = np.unravel_index(entry, selections.shape)
+
+    # N_ij = net change in i after unit progress in reaction j
+    # so the appropriate column of the stoich matrix tells us how to do our update
+    update = np.transpose(N[:,path_index])
+    update = update.reshape((N.shape[0],))
+    return update
+
+def expand_step_with_t_eval(t, y, hitting_time, update, t_eval, t_end):
+    # gather any intermediate values requested (y is constant at each intermediate)
+    t_history = list(t_eval[(t_eval > t) & (t_eval < t_end)])
+    t_history.append(t+hitting_time)
+    y_history = np.zeros((len(y), len(t_history)))
+    y_history[:,:len(t_history)-1] = y
+    y_history[:,len(t_history)-1]  = y+update
+
+    return t_history, y_history
+
+def gillespie_step(t, y, t_end, N, propensity_function, rng, t_eval):
+    hitting_time = find_hitting_time_inhomogenous(t, y, propensity_function, rng).tau
+    endpoint_propensities = propensity_function(t+hitting_time, y)
+    total_propensity = np.sum(endpoint_propensities)
+
+    update = gillespie_update_proposal(N, endpoint_propensities, total_propensity, rng)
+    t_history, y_history = expand_step_with_t_eval(t,y,hitting_time,update,t_eval,t_end)
+
+    return Step(t_history, y_history, GillespieStepStatus.stochastic)
+
+def homogeneous_gillespie_step(t, y, t_end, N, propensity_function, rng, t_eval):
+    propensities = propensity_function(t, y)
+    print("y", y)
+    total_propensity = np.sum(propensities)
+    time_proposal = find_hitting_time_homogeneous(propensities, total_propensity, rng)
+    hitting_time, propensities, total_propensity = time_proposal
+
+    if t + hitting_time > t_end:
+        update = np.zeros_like(y)
+        return Step(*expand_step_with_t_eval(t,y,t_end-t,update,t_eval,t_end), GillespieStepStatus.t_end)
+
+    update = gillespie_update_proposal(N, propensities, total_propensity, rng)
+    t_history, y_history = expand_step_with_t_eval(t,y,hitting_time,update,t_eval,t_end)
+
+    return Step(t_history, y_history, GillespieStepStatus.stochastic)
+
+class GillespieSimulator(Simulator):
+    def step(self, t, y, t_end, rng, t_eval):
+        if self.inhomogeneous:
+            return gillespie_step(t, y, t_end, self.N, self.propensity_function, rng, t_eval) 
+        else:
+            return homogeneous_gillespie_step(t, y, t_end, self.N, self.propensity_function, rng, t_eval)
+    
+    def construct_propensity_function(self, k, kinetic_order_matrix):
+        inhomogeneous = self.inhomogeneous
+        @jit(nopython=True)
+        def jit_calculate_propensities(t, y):
+            # Remember, we want total number of distinct combinations * k === rate.
+            # we want to calculate (y_i rate_involvement_ij) (binomial coefficient)
+            # for each species i and each reaction j
+            # sadly, inside a numba C function, we can't avail ourselves of scipy's binom,
+            # so we write this little calculator ourselves
+            intensity_power = np.zeros_like(kinetic_order_matrix)
+            for i in range(0, kinetic_order_matrix.shape[0]):
+                for j in range(0, kinetic_order_matrix.shape[1]):
+                    if y[i] < kinetic_order_matrix[i][j]:
+                        intensity_power[i][j] = 0.0
+                    elif y[i] == kinetic_order_matrix[i][j]:
+                        intensity_power[i][j] = 1.0
+                    else:
+                        intensity = 1.0
+                        for x in range(0, kinetic_order_matrix[i][j]):
+                            intensity *= (y[i] - x) / (x+1)
+                        intensity_power[i][j] = intensity
+            if inhomogeneous:
+                k_of_t = k(t)
+            else:
+                k_of_t = k
+            product_down_columns = np.ones(len(k_of_t))
+            for i in range(0, len(y)):
+                product_down_columns = product_down_columns * intensity_power[i]
+            return product_down_columns * k_of_t
+        return jit_calculate_propensities
 
 class SimulationResult(NamedTuple):
     t: float
@@ -63,69 +155,4 @@ def forward_time(y0: np.ndarray, t_span: list[float], k: np.ndarray, N: np.ndarr
     y_history = y_history[:,:history_index]
 
     return SimulationResult(t_history[-1], y_history[:, -1], t_history, y_history, n_stochastic)
-
-class StepUpdate(NamedTuple):
-    t_update: float
-    y_update: np.ndarray
-    was_stochastic_event: bool
-
-class HittingTimeProposal(NamedTuple):
-    tau: float
-    propensities: np.ndarray = None
-    total_propensity: float = None
-
-def find_hitting_time_inhomogenous(y, k, t, calculate_propensities, rng):
-    hitting_point = rng.exponential(1)
-    f = inhomogeneous_upper_bound_f_factory(t, hitting_point, calculate_propensities)
-    hitting_time = fsolve(f, x0=1)
-    return HittingTimeProposal(hitting_time)
-
-def find_hitting_time_homogeneous(propensities, total_propensity, rng):
-    hitting_point = rng.exponential(1)
-    hitting_time = hitting_point / total_propensity
-    return HittingTimeProposal(hitting_time, propensities, total_propensity)
-
-def inhomogeneous_upper_bound_f_factory(t, hitting_point, calculate_propensities):
-    # we need to solve for the hitting time
-    # which is the time when hitting_point / integral of propensities
-    # so we build an objective function that takes a zero when x = hitting_point / integral
-    # using numerical minimization we can minimize the objective function to find the time x
-    def objective_function(x):
-        integral = quad(calculate_propensities, t, x)[0]
-        return np.abs(x - hitting_point / integral)
-
-def gillespie_update_proposal(N, propensities, total_propensity, rng):
-    selections = propensities.cumsum() / total_propensity
-    pathway_rand = rng.random()
-    entry = np.argmax(selections > pathway_rand)
-    path_index = np.unravel_index(entry, selections.shape)
-
-    # N_ij = net change in i after unit progress in reaction j
-    # so the appropriate column of the stoich matrix tells us how to do our update
-    update = np.transpose(N[:,path_index])
-    update = update.reshape((N.shape[0],))
-    return update
-
-def gillespie_step(y, k, t, t_end, N, calculate_propensities, rng):
-    hitting_time = find_hitting_time_inhomogenous(y, k, t, calculate_propensities, rng).tau
-
-    endpoint_propensities = calculate_propensities(y, k, hitting_time)
-    total_propensity = np.sum(endpoint_propensities)
-
-    update = gillespie_update_proposal(N, endpoint_propensities, total_propensity, rng)
-
-    return StepUpdate(hitting_time, update, was_stochastic_event=True)
-
-def homogeneous_gillespie_step(y, k, t, t_end, N, calculate_propensities, rng):
-    propensities = calculate_propensities(y, k)
-    total_propensity = np.sum(propensities)
-    time_proposal = find_hitting_time_homogeneous(propensities, total_propensity, rng)
-    hitting_time, propensities, total_propensity = time_proposal
-
-    if t + hitting_time > t_end:
-        return StepUpdate(t_end - t, np.zeros_like(y), was_stochastic_event=False)
-
-    update = gillespie_update_proposal(N, propensities, total_propensity, rng)
-
-    return StepUpdate(hitting_time, update, was_stochastic_event=True)
 
--- hybrid/simulators.py
+++ hybrid/simulators.py
@@ -1,5 +1,103 @@
-from hybrid.hybrid import forward_time
+import numpy as np
+from numpy.typing import ArrayLike
+from typing import Callable, NamedTuple
+from enum import IntEnum
+from collections import Counter
 
-SIMULATORS = {
-    'hybrid': forward_time
-}
+class StepStatus(IntEnum):
+    # simulators will subclass this to introduce other statuses.
+    def was_rejected(self):
+        return self < 0
+
+class Run():
+    def __init__(self, t0, y0, history_length=1e6) -> None:
+        history_length = int(history_length)
+        self.history_index = 0
+        
+        self.status_counter = Counter()
+        
+        self.t_history = np.zeros(history_length)
+        self.y_history = np.zeros((y0.shape[0], history_length))
+
+        self.t_history[0]    = t0
+        self.y_history[:, 0] = y0
+
+    def get_t(self):
+        return self.t_history[self.history_index]
+
+    def get_y(self):
+        return self.y_history[:, self.history_index]
+
+    def current_state(self):
+        return self.get_t(), self.get_y()
+
+    def handle_step(self, step):
+        self.status_counter.update({step.status:1})
+        if step.status.was_rejected():
+            return self.get_t()
+
+        n_samples = len(step.t_history)
+        self.t_history[self.history_index+1:self.history_index+n_samples+1] = step.t_history
+        self.y_history[:, self.history_index+1:self.history_index+n_samples+1] = step.y_history
+        self.history_index += n_samples
+        print(self.get_t(), self.history_index)
+        return self.get_t()
+
+    def get_history(self):
+        t_history = self.t_history[:self.history_index+1]
+        y_history = self.y_history[:,:self.history_index+1]
+        return History(self.get_t(), self.get_y(), t_history, y_history, self.status_counter)
+
+class History(NamedTuple):
+    t: float
+    y: ArrayLike
+    t_history: ArrayLike
+    y_history: ArrayLike
+    status_counter: Counter
+
+class Step(NamedTuple):
+    t_history: ArrayLike
+    y_history: ArrayLike
+    status: StepStatus
+
+class Simulator():
+    run_klass = Run
+    def __init__(self, k, N, kinetic_order_matrix, propensity_function=None) -> None:
+        inhomogeneous = isinstance(k, Callable)
+        if not inhomogeneous:
+            k = np.asarray(k, dtype=float)
+        self.inhomogeneous = inhomogeneous
+
+        assert N.shape[0] == kinetic_order_matrix.shape[0], "N and kinetic_order_matrix should have # rows == # of species"
+        assert N.shape[1] == kinetic_order_matrix.shape[1], "N and kinetic_order_matrix should have # columns == # of reaction pathways"
+
+        self.k = k
+        self.N = N
+        self.kinetic_order_matrix = kinetic_order_matrix
+        if propensity_function is not None:
+            self.propensity_function = propensity_function
+        else:
+            self.propensity_function = self.construct_propensity_function(k, kinetic_order_matrix)
+
+    def initiate_run(self, t0, y0):
+        return self.run_klass(t0, y0)
+
+    def simulate(self, t_span: ArrayLike, y0: ArrayLike, rng: np.random.Generator, t_eval: ArrayLike=None):
+        assert self.N.shape[0] == self.kinetic_order_matrix.shape[0] == y0.shape[0], "N and kinetic_order_matrix should have # rows == # of species"
+        assert len(t_span) == 2
+        t0, t_end = t_span
+
+        if t_eval is None: t_eval = np.array([])
+
+        run = self.initiate_run(t0, y0)
+
+        t = t0
+        while t < t_end:
+            step = self.step(*run.current_state(), t_end, rng, t_eval)
+            t = run.handle_step(step)
+            #break
+
+        return run.get_history()
+
+class HybridSimulator(Simulator):
+    pass
