5d2879bfb4e19d996b59751acfe056a864c6b0a4
Thayer Anderson <tanderson11@gmail.com>
2024-05-01 16:36:54 -0400

    Factor out test discovery.

--- tests/submit_sbml.py
+++ tests/submit_sbml.py
@@ -7,7 +7,7 @@ import shlex
 import unit_tests
 
 if __name__ == '__main__':
-    assert len(sys.argv) <= 2, "Usage: 'python generate_submission_script maxjobs' where maxjobs == maximum jobs to run at once"
+    assert len(sys.argv) <= 2, "Usage: 'python submit_sbml.py maxjobs' where maxjobs == maximum jobs to run at once"
     if len(sys.argv) == 2:
         maxjobs = int(sys.argv[1])
     else:
