d998ed9b0ddc6bb247147050dd033f7a5ddcdabf
Thayer Anderson <tanderson11@gmail.com>
2024-05-03 13:05:36 -0400

    Fix maxjob calculation.

--- tests/submit_sbml.py
+++ tests/submit_sbml.py
@@ -1,10 +1,9 @@
 import sys
 import unittest
-import os
 import subprocess
 import shlex
 
-import unit_tests
+import sbml_tests
 
 if __name__ == '__main__':
     assert len(sys.argv) <= 2, "Usage: 'python submit_sbml.py maxjobs' where maxjobs == maximum jobs to run at once"
@@ -12,5 +11,5 @@ if __name__ == '__main__':
         maxjobs = int(sys.argv[1])
     else:
         maxjobs = 16
-    ntests = len(list(unittest.TestLoader().loadTestsFromTestCase(unit_tests.TestSBML)))
+    ntests = len(list(unittest.TestLoader().loadTestsFromTestCase(sbml_tests.TestSBML)))
     subprocess.run(shlex.split(f'sbatch -o slurm-%A_%a.out --array=0-{ntests-1}%{maxjobs} submit_sbml.sh'))
