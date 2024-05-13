da54c4fb5aaab79dfff9b5075b24ea12b6ae5528
Thayer Anderson <tanderson11@gmail.com>
2024-05-13 14:47:23 -0400

    Agnostic time dependent suite submission.

--- tests/suites.py
+++ tests/suites.py
@@ -1,8 +1,10 @@
 import tests.sbml.sbml_tests as sbml
 import tests.hybrid.run_hybrid as run_hybrid
+import tests.time_dependent.run_time_dependent as run_time_dependent
 
 suites = {
     'sbml': sbml.TestSBML,
     'decayingisom': run_hybrid.TestDecayingIsomerization,
     'schlogl': run_hybrid.TestSchlogl,
+    'timedependent': run_time_dependent.TestTimeDependent,
 }
