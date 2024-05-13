4318496a6d67a0049ad7a2f088b4a4779d5a7ed9
Thayer Anderson <tanderson11@gmail.com>
2024-05-13 14:34:52 -0400

    Copy env to command.

--- tests/submit.py
+++ tests/submit.py
@@ -2,6 +2,7 @@ import unittest
 import subprocess
 import shlex
 import argparse
+import os
 
 from tests.suites import suites
 
@@ -14,7 +15,9 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     ntests = len(list(unittest.TestLoader().loadTestsFromTestCase(suites[args.suite])))
+    env = os.environ.copy()
+    env['TEST_SUITE'] = args.suite
     subprocess.run(
         shlex.split(f'sbatch -o {args.suite}-%A_%a.out --array=0-{ntests-1}%{args.maxjobs} --jobname={args.suite} --time={args.time}:0:0 --mem={args.memory}G submit.sh'),
-        env={'TEST_SUITE': args.suite}
+        env=env
     )
