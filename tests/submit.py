d9e4a8cf82645fcdbafa3e1b73bf6d792643d2f7
Thayer Anderson <tanderson11@gmail.com>
2024-06-06 09:23:27 -0700

    Slight tweaks to simulators and submission.

--- tests/submit.py
+++ tests/submit.py
@@ -9,12 +9,13 @@ from tests.suites import suites
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('suite', choices=suites.keys(), help="name of test suite to run")
-    parser.add_argument('-n', '--maxjobs', type=int, default=16, help="index of test to run from suite, arbitrarily sorted")
-    parser.add_argument('-t', '--time', type=int, default=8, help="number of hours allocated for each job")
+    parser.add_argument('-n', '--maxjobs', type=int, default=64, help="maximum number of jobs to run at once")
+    parser.add_argument('-t', '--time', type=int, default=12, help="number of hours allocated for each job")
     parser.add_argument('-m', '--memory', type=int, default=16, help='gb of memory allocated for each job')
     args = parser.parse_args()
 
-    ntests = len(list(unittest.TestLoader().loadTestsFromTestCase(suites[args.suite])))
+    tests = list(unittest.TestLoader().loadTestsFromTestCase(suites[args.suite]))
+    ntests = len(tests)
     env = os.environ.copy()
     env['TEST_SUITE'] = args.suite
     subprocess.run(
